a3e83b7505fd720a7dbd6cb0c391452b9f4861e0
Bertus8 <albertinisp@gmail.com>
2022-02-08 16:11:24 +0100

    slider v0

--- src/pages/PreLogin/PreLogin.jsx
+++ src/pages/PreLogin/PreLogin.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useSwiper } from 'swiper/react';
+import "./PreLogin.scss";
 import 'swiper/css';
-import "./PreLogin.scss"
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const PreLogin = () => {
+    const swiper = useSwiper();
+
+
   return <div className="slider-container">
+      
          <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
+      slidesPerView={1}
+      onSlideChange={() => {/*...*/}}
+      onReachEnd={() => {/*...*/}}
     >
-      <SwiperSlide><img src="/images/slider/imagen1.png" alt=""/><h3>Encuentra todo tipo de servicios que tienes cerca de ti</h3></SwiperSlide>
-      <SwiperSlide><img src="/images/slider/imagen2.png" alt=""/>
+      <SwiperSlide>{({ isPrev }) => (<div><img src="/images/slider/imagen1.png" alt=""/><h3>Encuentra todo tipo de servicios que tienes cerca de ti</h3>{isPrev ? 'active' : 'not active'}</div>)}</SwiperSlide>
+      <SwiperSlide>{({ isActive }) => (<div><img src="/images/slider/imagen2.png" alt=""/>
       <h3>Adopta desde tu móvil</h3>
-      <p>Puedes acceder al perfil de muchos animales que están en adopción y filtrarlos para encontrar el que mejor se adapte a ti</p></SwiperSlide>
-      <SwiperSlide><img src="/images/slider/imagen3.png" alt=""/> <h3>Si eres una asociación sube a tus peludos para darles más difusión</h3></SwiperSlide>
-      ...
+      <p>Puedes acceder al perfil de muchos animales que están en adopción y filtrarlos para encontrar el que mejor se adapte a ti</p>{isActive ? 'active' : 'not active'}</div>)}</SwiperSlide>
+      <SwiperSlide>{({ isNext }) => (<div><img src="/images/slider/imagen3.png" alt=""/> <h3>Si eres una asociación sube a tus peludos para darles más difusión</h3>{isNext ? 'active' : 'not active'}</div>)}</SwiperSlide>
+      <div className='btn-contain'>
+      <button className='btn-slider' onClick={() => swiper.slideNext()}></button>
+      <button className='btn-slider' onClick={() => swiper.slideActive()}></button>
+      <button className='btn-slider' onClick={() => swiper.slidePrev()}></button>
+      </div>
     </Swiper>
   </div>;
 };
