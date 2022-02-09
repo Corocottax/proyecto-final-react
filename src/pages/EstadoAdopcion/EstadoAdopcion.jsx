6fa3e166b884024f81c61133c2505c79d624984c
Bertus8 <albertinisp@gmail.com>
2022-02-09 17:43:03 +0100

    estado de adopcion revisar peticiones

--- src/pages/EstadoAdopcion/EstadoAdopcion.jsx
+++ src/pages/EstadoAdopcion/EstadoAdopcion.jsx
@@ -1,36 +1,51 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import "./EstadoAdopcion.scss"
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Buscador from '../../shared/Buscador/Buscador.jsx'
 
 
 const EstadoAdopcion = () => {
   const [texto, setTexto] = useState("");
-  const [mascotas, setMascotas] = useState([]);
-  const UrlAnimales =
-  "https://proyecto-final-api-mocha.vercel.app/api/mascotas";
-  
-  useEffect(() => {
-    axios(UrlAnimales).then((res) => {
-      setMascotas(res.data);
+  const getUserById = (id) => {
+    return fetch(
+      `https://proyecto-final-api-mocha.vercel.app/api/users/${id}`
+    ).then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return [];
+      }
     });
-  }, []);
-
+  };
+    const [user, setUser] = useState([]);
+    let { id } = useParams("estadoAdopcion");
+    const [mascota, setMascota] = useState([]);
+  
+    useEffect(() => {
+      if (id)
+        getUserById(id).then((data) => {
+          setUser(data);
+          console.log(data);
+        });
+    }, []);
+    const mascotaFilter = mascota.filter((mascota) =>
+    mascota.name.toLowerCase().includes(texto.toLowerCase())
+  );
   return (<div>
     <div className="searcher-container">
     <Link to="/perfil"><img src="/images/flecha.png" alt="flecha"/></Link>
     <Buscador texto={texto} setTexto={setTexto} />
     <button className='btn-filter'><img src="/images/filtro.png" alt="filtro"/></button>
     </div>
-    <div>
+    {user.estadoAdopcion && user.estadoAdopcion.map((estadoAdopcion) => (
+      <div> 
       <span>
-      <h3>Adopcion de {mascotas.name}</h3>
-      <p>{mascotas}</p>
+      <h3>Adopcion de {estadoAdopcion.name}</h3>
+      <p></p>
       </span>
-      <img src={mascotas.photo} alt={mascotas.name} />
-    </div>
+      <img src={estadoAdopcion.photo} alt={estadoAdopcion.name} />
+    </div>))}
   </div>);
 };
 
