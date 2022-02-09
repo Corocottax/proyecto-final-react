83dbf224c0c983e03c3217ce28bd37386c66cdb1
unknown <ricardo.meraciudad@gmail.com>
2022-02-09 20:28:16 +0100

    feat first adoption filters

--- src/pages/Animales/Animales.jsx
+++ src/pages/Animales/Animales.jsx
@@ -7,9 +7,10 @@ import Buscador from "../../shared/Buscador/Buscador";
 import { Link } from "react-router-dom";
 import { Fade } from "react-awesome-reveal";
 
-const Animales = ({setNavbar}) => {
+const Animales = ({ setNavbar }) => {
   // AXIOS
-  const UrlAnimales ="https://proyecto-final-api-mocha.vercel.app/api/mascotas";
+  const UrlAnimales =
+    "https://proyecto-final-api-mocha.vercel.app/api/mascotas";
 
   const [mascotas, setMascotas] = useState([]);
   setNavbar(true);
@@ -30,55 +31,89 @@ const Animales = ({setNavbar}) => {
 
   return (
     <div>
-
       <div className="header">
-
-        <Link to="/home" className='volver'>
-          <img className="imgHeader" src="https://res.cloudinary.com/dhp2zuftj/image/upload/v1644422043/proyecto%20final/atras_3x_druibb.png" alt="flechita" />
+        <Link to="/home" className="volver">
+          <img
+            className="imgHeader"
+            src="https://res.cloudinary.com/dhp2zuftj/image/upload/v1644422043/proyecto%20final/atras_3x_druibb.png"
+            alt="flechita"
+          />
         </Link>
-        
-        <Buscador/>
+
+        <Buscador />
 
         {/* -----------------------BOTON Y POPUP PARA EL FILTRO-------------------------- */}
-        <img  onClick={(e) => setVisibility(!visibility)} className="imgfiltros" src="https://res.cloudinary.com/dhp2zuftj/image/upload/v1644422043/proyecto%20final/filtros_3x_etqcy3.png" alt="filtrado" />
-
+        <img
+          onClick={(e) => setVisibility(!visibility)}
+          className="imgfiltros"
+          src="https://res.cloudinary.com/dhp2zuftj/image/upload/v1644422043/proyecto%20final/filtros_3x_etqcy3.png"
+          alt="filtrado"
+        />
       </div>
 
-        <CustomPopup
-          className="filters-popup"
-          onClose={popupCloseHandler}
-          show={visibility}
-        >
-          <div className="ASDF">
-            <h1>hola</h1>
+      <CustomPopup
+        className="filters-popup"
+        onClose={popupCloseHandler}
+        show={visibility}
+      >
+        <div className="popup-filter">
+          <h2>Filtros</h2>
+
+          <div className="popup-filter--city">
+            <h3>Ciudad</h3>
+            <select name="select">
+              <option value="Elige tu zona" selected>
+                Elige tu zona
+              </option>
+              <option value="Madrid">Madrid</option>
+              <option value="Cataluña">Cataluña</option>
+            </select>
           </div>
-        </CustomPopup>
 
-    <div className="album">
-
-      {mascotas && mascotas.map((mascota, index) => {
-        
-        return(
-          <Fade className="carta" key={index} delay={200} triggerOnce>
-          <div key={index}>
-
-          <img className="imagen-carta" src={mascota.foto} alt={mascota.nombre} />
-          <div className="carta-detail">
-            <h2 className="nombre-carta">{mascota.nombre}</h2>
-            <p className="localidad-carta">{mascota.localizacion}</p>
+          <div className="popup-filter--especie">
+            <h3>Especie</h3>
+             <div className="popup-filter--especieButtons">
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+               <button class="button"> ASDF </button>
+             </div>
+            
           </div>
-          
+
+          <div></div>
         </div>
-        </Fade>)
-        
+      </CustomPopup>
 
-      })}
-
-    </div>
-
+      <div className="album">
+        {mascotas &&
+          mascotas.map((mascota, index) => {
+            return (
+              <Fade className="carta" key={index} delay={200} triggerOnce>
+                <div key={index}>
+                  <img
+                    className="imagen-carta"
+                    src={mascota.foto}
+                    alt={mascota.nombre}
+                  />
+                  <div className="carta-detail">
+                    <h2 className="nombre-carta">{mascota.nombre}</h2>
+                    <p className="localidad-carta">{mascota.localizacion}</p>
+                  </div>
+                </div>
+              </Fade>
+            );
+          })}
+      </div>
     </div>
   );
 };
 
 export default Animales;
-
