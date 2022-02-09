7f56b113b51c874ce22f08f66b090d809706c706
unknown <ricardo.meraciudad@gmail.com>
2022-02-09 10:59:55 +0100

    feat loading screen

--- src/shared/Splash/Splash.jsx
+++ src/shared/Splash/Splash.jsx
@@ -1,48 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { Bounce, Fade } from 'react-awesome-reveal';
-import PreLogin from '../../pages/PreLogin/PreLogin';
-import './Splash.scss';
+import React, { useState, useEffect } from "react";
+import { Bounce, Fade } from "react-awesome-reveal";
+import PreLogin from "../../pages/PreLogin/PreLogin";
+import "./Splash.scss";
 
 const Splash = () => {
-    const [showSplash, setShowSplash] = useState(0);
+  const [showSplash, setShowSplash] = useState(0);
 
-    const carga = () => {
+  const carga = () => {
+    setTimeout(() => {
+      setShowSplash(1);
+    }, 5000);
 
-      setTimeout(() => {
-        
-        setShowSplash(1);
+    setShowSplash(0);
+  };
 
-      }, 3000);
-
-      setShowSplash(0);
-
-    }
-
-    useEffect(() => {
-        
-      carga();
-
-    }, []);
+  useEffect(() => {
+    carga();
+  }, []);
 
   return (
     <>
-        {showSplash === 0 &&  <div className="pantallaDeCarga">
-        <Bounce>
-          <img
-            src="https://res.cloudinary.com/ddbvk5mrr/image/upload/v1644320679/App-Pet/logo_hyfwge.png"
-            alt="logo"
-          />
-        </Bounce>
-        <Fade delay={1000} triggerOnce>
-          <img
-            src="https://res.cloudinary.com/ddbvk5mrr/image/upload/v1644320680/App-Pet/title_u1r9rk.png"
-            alt="title"
-          />
-        </Fade>
-    </div>}
-        
+      {showSplash === 0 && (
+        <div className="pantallaDeCarga">
+  
+          <Bounce>
+            <img
+              src="https://res.cloudinary.com/ddbvk5mrr/image/upload/v1644320679/App-Pet/logo_hyfwge.png"
+              alt="logo"
+            />
+          </Bounce>
+          <Fade delay={800} triggerOnce>
+            <img
+              src="https://res.cloudinary.com/ddbvk5mrr/image/upload/v1644320680/App-Pet/title_u1r9rk.png"
+              alt="title"
+            />
+          </Fade>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Splash
+export default Splash;
