3c15bf4ef32de16c9d73212a150afdd2c32ebe4c
Bertus8 <albertinisp@gmail.com>
2022-02-10 09:50:08 +0100

    filtro de estado

--- src/pages/PopupFiltroDeEstado/PopupFiltroDeEstado.jsx
+++ src/pages/PopupFiltroDeEstado/PopupFiltroDeEstado.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import CustomPopup from "../../shared/PopUp/PopUp";
 import "./PopupFiltroDeEstado.scss"
 
 const PopupFiltroDeEstado = () => {
-  return <div>
+  return (
+<>
+<CustomPopup
+        onClose={popupCloseHandler}
+        show={visibility}
+      >
+        <div className='container-filter'>
+          <h3 className='title-filter'>Filtros</h3>
+          <div className='container-btn-filter'>
+          <button className='btn-filter'><img src="/images/completado.png" alt="completado" /></button>
+          <button className='btn-filter'><img src="/images/proceso.png" alt="en proceso" /></button>
+          <button className='btn-filter'><img src="/images/rechazado.png" alt="rechazado" /></button>
+          </div>
+          <button onClick={submit} className='btn-submit'>Aplicar</button>
+        </div>
 
-  </div>;
+
+      </CustomPopup>
+      </>)
 };
 
 export default PopupFiltroDeEstado;
