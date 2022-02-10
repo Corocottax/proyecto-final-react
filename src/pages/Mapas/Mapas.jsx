c68450a732fba69f8087f54a504d2d59a100cb15
unknown <santicorocottax@gmail.com>
2022-02-10 18:05:17 +0100

    intento mapas

--- src/pages/Mapas/Mapas.jsx
+++ src/pages/Mapas/Mapas.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./Mapas.scss"
 
 const Mapas = ({setNavbar}) => {
 
   setNavbar(true);
 
-  return <div></div>;
+  useEffect(() => {
+
+    new window.google.maps.Map(document.getElementById("map"), {
+
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+
+      });
+}, [])
+
+const mapStyleS = {height: '500px'}
+return (
+    <div className="App">
+        <div style={mapStyleS} id="map"/>
+    </div>
+);
 };
 
 export default Mapas;
